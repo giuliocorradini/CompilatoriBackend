4eeef3c252a326ad3eb6b7839a01b4d5094fe94d
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-06-13 11:02:32 +0200

    Assignment4 test files

---
+++ Lab/Assignment4/Loop.c
@@ -0,0 +1,8 @@
+void loops(int *a, int *b, int *c, int *d) {
+    int TRIPS = 100;
+    for (int i=0; i<TRIPS; i++)
+        a[i] = b[i] * c[i];
+
+    for (int i=0; i<TRIPS; i++)
+        d[i] = a[i+3] + c[i];
+}
